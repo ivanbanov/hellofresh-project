1b25bd40cbe9de34c93839f1b5228375cc5e741e
Ivan Banov <ivanbanov@gmail.com>
2017-04-01 15:43:26 -0300

    Use authentication on api

--- server/api.js
+++ server/api.js
@@ -9,6 +9,7 @@ const USER = 'user@hellofresh.com';
 const PASSWORD = '123';
 
 const PORT = 3001;
+const TOKEN = 'foo-bar';
 
 app.use(morgan('tiny'));
 
@@ -16,31 +17,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('HelloFresh Api')
+  res.status(200).send('HelloFresh Api');
 });
 
 app.post('/login', (req, res) => {
   if (req.body.user === USER && req.body.password === PASSWORD) {
-    return res.send({
+    return res.status(200).send({
       status: 'ok',
-      token: 'foo-bar',
+      token: TOKEN,
     });
   }
 
-  return res.send({
+  return res.status(401).send({
     status: 'error',
     message: 'Invalid user and/or password ',
   });
 });
 
 app.get('/recipes', (req, res) => {
-  return res.send({
-    status: 'ok',
-    data: FIXTURE,
+  if (req.headers.token === TOKEN) {
+    return res.status(200).send({
+      status: 'ok',
+      data: FIXTURE,
+    });
+  }
+
+  return res.status(403).send({
+    status: 'error',
+    data: 'User not authenticated',
   });
 });
 
-app.listen(PORT, 'localhost', err => {
+app.listen(PORT, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
