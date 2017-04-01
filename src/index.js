55844598660e8568241f49ddd32e09f5b484a222
Ivan Banov <ivanbanov@gmail.com>
2017-03-31 22:34:40 -0300

    Setup react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import './styles.styl';
 
 class App extends React.Component {
   render() {
-    return <h1>HelloFresh</h1>;
+    return (
+      <Router>
+        <div>
+          <Route path="/login" component={() => (<h1>Login</h1>)} />
+          <Route path="/recipes" component={() => (<h1>Recipes</h1>)} />
+        </div>
+      </Router>
+    );
   }
 }
 
